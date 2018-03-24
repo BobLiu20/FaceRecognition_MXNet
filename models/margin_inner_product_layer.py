[bugfix]re-design a-softmax

models/margin_inner_product_layer.py
@@ -3,112 +3,83 @@ sys.path.insert(0, "/home/liubofang/bob_opensource/incubator-mxnet-v1.1.0/python
 import mxnet as mx
 from mxnet import nd
 from mxnet.gluon import nn
+import math
 
 class MarginInnerProduct(nn.Block):
     def __init__(self, margin_params, **kwargs):
         super(MarginInnerProduct, self).__init__(**kwargs)
         #  args
-        self.ctx_list = []
-        self.in_units = margin_params["in_units"]
-        self.out_units = margin_params["out_units"]
+        self.in_units = margin_params.get("feature_dim", 512)
+        self.out_units = margin_params["label_num"]
         #  lambda parameter
-        self.lamb_iter = margin_params["lamb_iter"]
-        self.lamb_base = margin_params["lamb_base"]
-        self.lamb_gamma = margin_params["lamb_gamma"]
-        self.lamb_power = margin_params["lamb_power"]
-        self.lamb_min = margin_params["lamb_min"]
+        self.lamb_iter = margin_params.get("lamb_iter", 0)
+        self.lamb_base = margin_params.get("lamb_base", 1500)
+        self.lamb_gamma = margin_params.get("lamb_gamma", 0.01)
+        self.lamb_power = margin_params.get("lamb_power", 1)
+        self.lamb_min = margin_params.get("lamb_min", 10)
+        #  margin type
+        self.margin = margin_params.get("margin", 4)
+        self.margin_cos = [
+            lambda x: x**0,
+            lambda x: x**1,
+            lambda x: 2 * x**2 - 1,
+            lambda x: 4 * x**3 - 3 * x,
+            lambda x: 8 * x**4 - 8 * x**2 + 1,
+            lambda x: 16 * x**5 - 20 * x**3 + 5 * x
+        ]
         #  training parameter
         with self.name_scope():
-            self.weight = self.params.get('weight', shape=(self.out_units, self.in_units))
-        #  global variable
-        self.x_norm = []
-        self.cos_theta = []
-        self.sign_0 = []
-        self.sign_3 = []
-        self.sign_4 = []
-        self.cos_theta_quadratic = []
-        self.cos_theta_cubic = []
-        self.cos_theta_quartic = []
-        self.nd_1 = []
-        self.nd_2 = []
-        self.nd_3 = []
-        self.nd_4 = []
+            self.weight = self.params.get('weight', shape=(self.in_units, self.out_units))
 
     def forward(self, x, label):
+        #  weight normalize
+        with x.context:
+            w = self.weight.data()
         with mx.autograd.pause():
             batch_size = x.shape[0]
-            ctx = x.context
-            self.__init_all_tensor(batch_size, ctx)
-            idx = self.ctx_list.index(ctx)
-            _lambda = self.__get_lambda()
             #  normalize weight
-            self.weight.data(ctx)[:] = mx.ndarray.L2Normalization(self.weight.data(ctx),
-                                                                  mode='instance')
-            #  x_norm = |x|
-            for i in range(batch_size):
-                nd.norm(x[i], out=self.x_norm[idx][i])
-            #  cos_theta = x'w/|x|
-            nd.dot(x, self.weight.data(ctx), False, True, self.cos_theta[idx])
-            self.cos_theta[idx] /= self.x_norm[idx].reshape((-1, 1))
-            #  sign_0 = sign(cos_theta)
-            nd.sign(self.cos_theta[idx], out=self.sign_0[idx])
-
-            ###  MarginInnerProductParameter_MarginType_QUADRUPLE
-            #  cos_theta_quadratic && cos_theta_cubic && cos_theta_quartic
-            nd.broadcast_power(self.cos_theta[idx], self.nd_2[idx],
-                               out=self.cos_theta_quadratic[idx])
-            # nd.broadcast_power(self.cos_theta[idx], self.nd_3[idx], out=self.cos_theta_cubic[idx])
-            nd.broadcast_power(self.cos_theta[idx], self.nd_4[idx], out=self.cos_theta_quartic[idx])
-            #  sign_3 = sign_0 * sign(2 * cos_theta_quadratic_ - 1)
-            nd.broadcast_mul(self.cos_theta_quadratic[idx], self.nd_2[idx], out=self.sign_3[idx])
-            nd.broadcast_sub(self.sign_3[idx], self.nd_1[idx], out=self.sign_3[idx])
-            nd.sign(self.sign_3[idx], out=self.sign_3[idx])
-            nd.broadcast_mul(self.sign_0[idx], self.sign_3[idx], out=self.sign_3[idx])
-            #  sign_4 = 2 * sign_0 + sign_3 - 3
-            nd.broadcast_mul(self.sign_0[idx], self.nd_2[idx], out=self.sign_4[idx])
-            nd.broadcast_add(self.sign_4[idx], self.sign_3[idx], out=self.sign_4[idx])
-            nd.broadcast_sub(self.sign_4[idx], self.nd_3[idx], out=self.sign_4[idx])
-        #  Forward
-        x = nd.dot(x, self.weight.data(ctx), False, True)
-        #  + lambda * x'w
-        x_tmp = x * _lambda
-        #  |x| * (sign_3 * (8 * cos_theta_quartic - 8 * cos_theta_quadratic + 1) + sign_4)
-        tmp = self.x_norm[idx].reshape((-1, 1)) * (self.sign_3[idx] * \
-                (8.0 * self.cos_theta_quartic[idx] - 8.0 * self.cos_theta_quadratic[idx] + 1.0)\
-                + self.sign_4[idx])
-        oh = nd.one_hot(label, x.shape[1])
-        x = x * (1.0 - oh) + tmp * oh
-        x = x + x_tmp
-        #  / (1 + lambda)
-        x = x / (1 + _lambda)
-        return x
-
-    def __init_all_tensor(self, batch_size, ctx):
-        if ctx in self.ctx_list:
-            return
-        self.ctx_list.append(ctx)
-        self.x_norm.append(nd.zeros((batch_size,), ctx=ctx))
-        self.cos_theta.append(nd.zeros((batch_size, self.out_units), ctx=ctx))
-        self.sign_0.append(nd.zeros((batch_size, self.out_units), ctx=ctx))
-        self.sign_3.append(nd.zeros((batch_size, self.out_units), ctx=ctx))
-        self.sign_4.append(nd.zeros((batch_size, self.out_units), ctx=ctx))
-        self.cos_theta_quadratic.append(nd.zeros((batch_size, self.out_units), ctx=ctx))
-        self.cos_theta_cubic.append(nd.zeros((batch_size, self.out_units), ctx=ctx))
-        self.cos_theta_quartic.append(nd.zeros((batch_size, self.out_units), ctx=ctx))
-        self.nd_1.append(nd.array([1], ctx=ctx))
-        self.nd_2.append(nd.array([2], ctx=ctx))
-        self.nd_3.append(nd.array([3], ctx=ctx))
-        self.nd_4.append(nd.array([4], ctx=ctx))
+            with x.context:
+                w[:] = mx.ndarray.L2Normalization(w, mode='instance')
+        #  cos_theta = x'w/|x|. note: |w| = 1
+        x_norm = nd.power(x, 2)
+        x_norm = nd.sum(x_norm, axis=1)
+        x_norm = nd.sqrt(x_norm)
+        output = nd.dot(x, w)
+        cos_theta = output / x_norm.reshape((-1, 1))
+        cos_theta = nd.clip(cos_theta, -1, 1)
+        #  cos_m_theta = cos(m * theta)
+        cos_m_theta = self.margin_cos[self.margin](cos_theta)
+        #  k
+        with mx.autograd.pause():
+            theta = nd.arccos(cos_theta)
+            k = nd.sign((self.margin * theta / math.pi))
+        #  i=j is phi_theta and i!=j is cos_theta
+        phi_theta = ((-1)**k) * cos_m_theta - 2 * k
+        x_norm_phi_theta = x_norm.reshape((-1, 1)) * phi_theta
+        #  i=j index
+        with mx.autograd.pause():
+            index = nd.one_hot(label, output.shape[1])
+        #  output
+        with mx.autograd.pause():
+            lamb = self.__get_lambda()
+        output2 = output * (1.0 - index) + x_norm_phi_theta * index
+        output3 = (output2 + lamb * output) / (1 + lamb)
+        return output3
 
     def __get_lambda(self):
         self.lamb_iter += 1
-        return self.lamb_base * (1.0 + self.lamb_gamma * self.lamb_iter) ** (-self.lamb_power)
+        val = self.lamb_base * (1.0 + self.lamb_gamma * self.lamb_iter) ** (-self.lamb_power)
+        val = max(self.lamb_min, val)
+        if self.lamb_iter % 500 == 0:
+            print ("Now lambda = {}".format(val))
+        return val
 
 
 if __name__ == "__main__":
-    params = {"in_units": 4, "out_units": 6, 
+    params = {"feature_dim": 4, "class_num": 6, 
               "lamb_iter":0,"lamb_base":1000,"lamb_gamma":0.0001,"lamb_power":1,"lamb_min":10}
-    ctx = [mx.gpu(4), mx.gpu(5)]
+    # ctx = [mx.gpu(4), mx.gpu(5)]
+    ctx = [mx.gpu(1)]
     test = MarginInnerProduct(params)
     test.params
     test.initialize(ctx=ctx)
@@ -121,5 +92,5 @@ if __name__ == "__main__":
             y = test(x, label)
     print "y", y
     # print y
-    # y.backward()
+    y.backward()
 

models/sphere_face_net.py
@@ -27,10 +27,11 @@ class CNNResidualBlock(nn.Block):
         return x
 
 class SphereFaceNet(gluon.Block):
-    def __init__(self, feature_dim, label_num, model_params, **kwargs):
+    def __init__(self, model_params, **kwargs):
         super(SphereFaceNet, self).__init__(**kwargs)
         with self.name_scope():
-            self.models = self._main_net(model_params["layer_type"], feature_dim, label_num)
+            self.models = self._main_net(model_params["layer_type"], model_params["feature_dim"],
+                                         model_params["label_num"])
             #  A-softmax = margin_inner_product + softmax_loss
             self.margin_inner_product = MarginInnerProduct(model_params)
             self.softmax_loss = loss.SoftmaxCrossEntropyLoss()
@@ -56,7 +57,7 @@ class SphereFaceNet(gluon.Block):
 
 
 if __name__ == "__main__":
-    margin_params = {"in_units": 512, "out_units": 6, "lamb_iter": 0, "lamb_base": 1000,
+    margin_params = {"feature_dim": 512, "label_num": 6, "lamb_iter": 0, "lamb_base": 1000,
                      "lamb_gamma": 0.12, "lamb_power": 1, "lamb_min": 10}
     margin_params["layer_type"] = "20layer"
     test = SphereFaceNet(512, 6, margin_params)